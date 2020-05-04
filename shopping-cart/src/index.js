152f376547bc20d6bf23c361a59261a0ab6bf691
Stefano Saffran <stefanosaffran@hotmail.com>
2020-05-04 10:57:29 +0200

    chore: some minor updates

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -1,15 +1,12 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
-
-const getPromotion = products => promotions[[...new Set(products.map(product => product.category))].length - 1];
+const getPromotion = products => promotions[[new Set(products.map(product => product.category)).size - 1]];
 
 const getCartProducts = (ids, productsList) => productsList.filter(product => ids.includes(product.id));
 
 const getDiscount = (regularPrice, totalPrice) => 
 	Intl.NumberFormat('pt-BR', {
-		style: 'percent',
-		minimumFractionDigits: 2,
-		maximumFractionDigits: 2
+		style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2
 	}).format(1 - totalPrice / regularPrice);
 
 const getTotalPriceWithoutDiscount = products =>
@@ -17,18 +14,10 @@ const getTotalPriceWithoutDiscount = products =>
 		.map(product => product.regularPrice)
 		.reduce((sum, next) => sum + next, 0);
 
-
 const getTotalPrice = (promotion, products) => 
 	products.reduce((totalPrice, nextProduct) => {
-		const PromotionPrice = nextProduct.promotions.filter(promo => promo.looks.includes(promotion));
-
-		if(PromotionPrice[0]) {
-			totalPrice += PromotionPrice[0].price;
-		} else {
-			totalPrice += nextProduct.regularPrice;
-		}
-
-		return totalPrice;
+		const promotionPrice = nextProduct.promotions.find(promo => promo.looks.includes(promotion)) || [];
+		return totalPrice += promotionPrice.price || nextProduct.regularPrice;
 	}, 0)
 
 const mapProductToNameAndCategory = products =>
@@ -38,22 +27,14 @@ const mapProductToNameAndCategory = products =>
 	}))
 
 const getShoppingCart = (ids, productsList) => {
-
 	const cartProducts = getCartProducts(ids, productsList);
-
 	const promotion = getPromotion(cartProducts);
-
 	const discount = getDiscount(getTotalPriceWithoutDiscount(cartProducts), getTotalPrice(promotion, cartProducts));
-
 	const discountValue = (
 		getTotalPriceWithoutDiscount(cartProducts) - getTotalPrice(promotion, cartProducts)
 	).toFixed(2);
-	
 	const totalPrice = Intl.NumberFormat().format(getTotalPrice(promotion, cartProducts));
-
 	const products = mapProductToNameAndCategory(cartProducts);
-
-	console.log(promotion, discount, discountValue, totalPrice, products);
 
 	return {
 		products,
